a6f455691c832595890343260b5670c47831010e
emmassr <emma.ssr@gmail.com>
2019-11-01 16:42:28 +0000

    WIP:Access API for geolocation and weather summary

--- lib/weather.py
+++ lib/weather.py
@@ -1,5 +1,15 @@
+import requests
+import os
+
+
 class Weather(object):
     """docstring for Numbers."""
 
-    def get_forecast(self, location):
-        return "Cloudy"
+    def get_forecast(self, latitude, longitude):
+        darksky_response = requests.get(f"https://api.darksky.net/forecast/{os.environ['DARK_SKY_API_KEY']}/{latitude},{longitude}?exclude=[minutely,hourly,daily,alerts,flags]")
+        return darksky_response.json()["currently"]["summary"]
+
+
+    def get_latitude_and_longitude(self, city_name):
+        mapbox_response = requests.get(f"https://api.mapbox.com/geocoding/v5/mapbox.places/{city_name}.json?access_token={os.environ['MAPBOX_API_KEY']}")
+        return mapbox_response.json()["features"][0]["center"]
--- test/test_weather.py
+++ test/test_weather.py
@@ -1,4 +1,12 @@
 from lib.weather import Weather
 def test_gets_forecast_when_given_location():
     weather = Weather()
-    assert weather.get_forecast("London") == "Cloudy"
+    assert weather.get_forecast("51.528308","-0.3817826") == "Cloudy"
+
+def test_gets_forecast_when_given_different_location():
+    weather = Weather()
+    assert weather.get_forecast("52.48142","-1.89983") == "Sunny"
+
+def test_gets_longitude_latitude_from_city_name():
+    weather = Weather()
+    assert weather.get_latitude_and_longitude("Birmingham") == [-1.9,52.48333]
